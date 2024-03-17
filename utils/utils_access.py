c608066a629c042c587112839a60c1f8cf97a894
Dmitriy Kargin <dmitriy63kargin@gmail.com>
2024-03-18 01:55:55 +0400

    access folders mode progress pt.4

--- utils/utils_access.py
+++ utils/utils_access.py
@@ -1,4 +1,5 @@
 from load_all import bot
+from utils.utils_data import get_accesses_from_user_collection
 
 
 async def get_user_info(tg_user_id: str):
@@ -18,3 +19,10 @@ def get_access_str_by_type(access_type: str):
     elif access_type[0] == 'w':
         access_str += ' и изменению содержимого'
     return access_str
+
+
+async def check_access_folder_from_user(user_id, from_user_id, folder_id, access_type):
+    accesses_from_user_collection = await get_accesses_from_user_collection(user_id, from_user_id)
+    access_folder_collection = accesses_from_user_collection.get(folder_id, {})
+    #временный return
+    return access_folder_collection is False
