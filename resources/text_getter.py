aec04a6e142dc3384c38a5f3b550be3c458ff743
Dmitriy Kargin <dmitriy63kargin@gmail.com>
2024-04-28 11:55:55 +0400

    some review

--- resources/text_getter.py
+++ resources/text_getter.py
@@ -27,8 +27,7 @@ async def get_start_first_text(user: User):
     hello = _get_text(param='Hello', language=language)
     lets_get_started = _get_text(param='lets_get_started', language=language)
     start_first = _get_text(param='start_first', language=language)
-    start_first_text = (f"👋 {hello}, {user.first_name}, {lets_get_started}! 🚀️"
-                        f"{start_first}")
+    start_first_text = f"👋 {hello}, {user.first_name}, {lets_get_started}! 🚀️{start_first}"
     return start_first_text
 
 
